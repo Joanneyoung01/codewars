d49d49a2285fa9481d94561f4b58870f1321adca
joanne.young <joanne.young@beamly.com>
2019-12-13 15:47:06 +0000

    01-lost-without-a-map

---
+++ 01-lost-without-a-map/index.js
@@ -0,0 +1,16 @@
+/*
+  Title:
+    Beginner - Lost Without a Map
+  Description:
+    Given and array of integers (x), return the array with each value doubled.
+    For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+  Examples:
+    [1, 2, 3] --> [2, 4, 6]
+  Kata Link:
+    https://www.codewars.com/kata/beginner-lost-without-a-map
+*/
+
+//Solution
+function maps(x){
+  return x.map(n => n * 2);
+}
